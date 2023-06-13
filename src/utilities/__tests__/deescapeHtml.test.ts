dab7cbdacba6761b5f7a1219b5e1044622baa347
Ondrej Mladek <ondrej.mladek@gmail.com>
2023-06-13 11:59:01 +0200

    add tests

---
+++ src/utilities/__tests__/deescapeHtml.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+import deescapeHtml from '../deescapeHtml'
+
+describe('deescapeHtml', () => {
+  it('deescapes characters', () => {
+    expect(
+      deescapeHtml(`Foo &amp; bar, &lt;a&gt;link&lt;/a&gt; &quot; &#039;`),
+    ).eq(`Foo & bar, <a>link</a> " '`)
+  })
+})
