2bb1a683a2e790175a763bdde53863b86a53d57d
Ondrej Mladek <ondrej.mladek@gmail.com>
2023-06-15 12:57:33 +0200

    optimalize search suggestions

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,12 +1,19 @@
-import { FormEventHandler } from 'react'
+import { FormEventHandler, useState, useEffect } from 'react'
+import debounce from 'lodash.debounce'
+import getUniqueStrings from '../utilities/getUniqueStrings'
+import { useQuery } from '@tanstack/react-query'
+import { SearchResult } from '../types/reddit-api/SearchResult.type'
 
-const mergeAndUnique = (arr: string[]): string[] => {
-  const mergedOrdered = [...arr].sort((a, b) => a.localeCompare(b, 'en-US'))
-
-  return [...new Set(mergedOrdered)]
-    .filter(Boolean)
-    .filter((value) => value !== '')
-}
+const getOptions = (arr: string[]) =>
+  getUniqueStrings(
+    [...arr]
+      .sort((a, b) => a.localeCompare(b, 'en-US'))
+      .filter(Boolean)
+      .filter((option) => option !== ''),
+  ).map((option) => ({
+    value: option,
+    lowerCase: option.toLowerCase(),
+  }))
 
 interface Props {
   subreddits: string[]
@@ -14,8 +21,42 @@ interface Props {
 }
 
 const Filters = ({ subreddits, onSubmit }: Props): JSX.Element => {
-  const userSubreddits = (import.meta.env.VITE_SUBREDDITS || 'best').split(',')
-  const options = mergeAndUnique([...subreddits, ...userSubreddits])
+  const [subreddit, setSubreddit] = useState('')
+  const [optionsCache, setOptionsCache] = useState<
+    Array<{
+      value: string
+      lowerCase: string
+    }>
+  >(() => {
+    const userSubreddits: string[] = (
+      import.meta.env.VITE_SUBREDDITS || 'best'
+    ).split(',')
+
+    return getOptions([...userSubreddits, ...subreddits])
+  })
+  const { data } = useQuery<SearchResult, { message: string; reason?: string }>(
+    {
+      queryKey: ['subreddit-search', subreddit],
+      queryFn: ({ signal }) =>
+        fetch(
+          `https://www.reddit.com/search/.json?q=${subreddit}&type=sr&raw_json=1`,
+          { signal },
+        ).then((response) => response.json()),
+    },
+  )
+
+  useEffect(() => {
+    if (!data?.data?.children?.length) {
+      return
+    }
+
+    setOptionsCache((prev) => {
+      return getOptions([
+        ...prev.map((option) => option.value),
+        ...data.data.children.map((result) => result.data.display_name),
+      ])
+    })
+  }, [data?.data?.children])
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event): void => {
     event.preventDefault()
@@ -28,8 +69,26 @@ const Filters = ({ subreddits, onSubmit }: Props): JSX.Element => {
       return
     }
 
+    window.scrollTo({ top: 0 })
+
     onSubmit(value)
   }
+
+  const handleInput: FormEventHandler<HTMLInputElement> = debounce((event) => {
+    const value = event.target.value.trim()
+
+    if (value.length < 3) {
+      return
+    }
+
+    if (
+      optionsCache.find((option) => option.lowerCase === value.toLowerCase())
+    ) {
+      return
+    }
+
+    setSubreddit(value)
+  }, 300)
 
   return (
     <footer className="filters">
@@ -45,11 +104,14 @@ const Filters = ({ subreddits, onSubmit }: Props): JSX.Element => {
             name="subreddit"
             defaultValue="my-mix"
             placeholder="search subreddit"
+            onInput={handleInput}
+            autoComplete="off"
+            type="text"
           />
           <datalist id="subreddits">
             <option value="my-mix">my-mix</option>
-            {options.map((subreddit) => (
-              <option key={subreddit} value={subreddit} />
+            {optionsCache.map((subreddit) => (
+              <option key={subreddit.lowerCase} value={subreddit.value} />
             ))}
           </datalist>
           <button
--- src/utilities/getUniqueStrings.ts
+++ src/utilities/getUniqueStrings.ts
@@ -1,15 +1,18 @@
 const getUniqueStrings = (inputArray: string[]): string[] => {
   const uniqueStrings: string[] = []
-
-  // Convert all strings to lowercase for case-insensitive comparison
-  const lowerCaseStrings = inputArray.map((str) => str.toLowerCase())
+  const lowerCaseSet: Set<string> = new Set()
 
   // Iterate through each string in the input array
-  for (const str of lowerCaseStrings) {
-    // Check if the lowercase version of the string is already in the uniqueStrings array
-    if (!uniqueStrings.includes(str)) {
-      // Add the original string (not the lowercase version) to the uniqueStrings array
-      uniqueStrings.push(inputArray[lowerCaseStrings.indexOf(str)])
+  for (const str of inputArray) {
+    const lowerCaseString = str.toLowerCase()
+
+    // Check if the lowercase version of the string is already in the lowerCaseSet
+    if (!lowerCaseSet.has(lowerCaseString)) {
+      // Add the original string to the uniqueStrings array
+      uniqueStrings.push(str)
+
+      // Add the lowercase string to the lowerCaseSet
+      lowerCaseSet.add(lowerCaseString)
     }
   }
 
