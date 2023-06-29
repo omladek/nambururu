6cc95417a2c30d7390b2a18670df45a526dc5e40
Ondrej Mladek <ondrej.mladek@gmail.com>
2023-06-29 15:43:40 +0200

    better routing folder structure

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -33,69 +33,66 @@ function Editor(): JSX.Element {
   }
 
   return (
-    <main className="main">
-      <h1 className="title">Settings</h1>
-      <form className="editor" onSubmit={handleSubmit}>
-        <fieldset className="editor__fieldset">
-          <label htmlFor={Storage.MY_MIX}>my mix</label>
-          <p>comma separated subreddit names</p>
-          <textarea
-            defaultValue={myMix}
-            id={Storage.MY_MIX}
-            name={Storage.MY_MIX}
-            onChange={() => setIsSaved(false)}
-          />
-        </fieldset>
+    <form className="editor" onSubmit={handleSubmit}>
+      <fieldset className="editor__fieldset">
+        <label htmlFor={Storage.MY_MIX}>my mix</label>
+        <p>comma separated subreddit names</p>
+        <textarea
+          defaultValue={myMix}
+          id={Storage.MY_MIX}
+          name={Storage.MY_MIX}
+          onChange={() => setIsSaved(false)}
+        />
+      </fieldset>
 
-        <fieldset className="editor__fieldset">
-          <label htmlFor={Storage.MY_SELECTION}>my selection</label>
-          <p>comma separated subreddit names</p>
-          <textarea
-            defaultValue={mySelection}
-            id={Storage.MY_SELECTION}
-            name={Storage.MY_SELECTION}
-            onChange={() => setIsSaved(false)}
-          />
-        </fieldset>
+      <fieldset className="editor__fieldset">
+        <label htmlFor={Storage.MY_SELECTION}>my selection</label>
+        <p>comma separated subreddit names</p>
+        <textarea
+          defaultValue={mySelection}
+          id={Storage.MY_SELECTION}
+          name={Storage.MY_SELECTION}
+          onChange={() => setIsSaved(false)}
+        />
+      </fieldset>
 
-        <fieldset className="editor__fieldset">
-          <label htmlFor={Storage.MY_BLOCKED_SUBREDDITS}>
-            my blocked subreddits
-          </label>
-          <p>
-            comma separated subreddit names which should not be shown in the
-            best/top/hot list(s)
-          </p>
-          <textarea
-            defaultValue={myBlockedSubreddits}
-            id={Storage.MY_BLOCKED_SUBREDDITS}
-            name={Storage.MY_BLOCKED_SUBREDDITS}
-            onChange={() => setIsSaved(false)}
-          />
-        </fieldset>
+      <fieldset className="editor__fieldset">
+        <label htmlFor={Storage.MY_BLOCKED_SUBREDDITS}>
+          my blocked subreddits
+        </label>
+        <p>
+          comma separated subreddit names which should not be shown in the
+          best/top/hot list(s)
+        </p>
+        <textarea
+          defaultValue={myBlockedSubreddits}
+          id={Storage.MY_BLOCKED_SUBREDDITS}
+          name={Storage.MY_BLOCKED_SUBREDDITS}
+          onChange={() => setIsSaved(false)}
+        />
+      </fieldset>
 
-        <fieldset className="editor__fieldset">
-          <label htmlFor={Storage.MY_BLOCKED_TITLE_KEYWORDS}>
-            my blocked title keywords
-          </label>
-          <p>
-            comma separated words; if a post title includes this word(s) it will
-            not be shown
-          </p>
-          <textarea
-            defaultValue={myBlockedTitleKeywords}
-            id={Storage.MY_BLOCKED_TITLE_KEYWORDS}
-            name={Storage.MY_BLOCKED_TITLE_KEYWORDS}
-            onChange={() => setIsSaved(false)}
-          />
-        </fieldset>
+      <fieldset className="editor__fieldset">
+        <label htmlFor={Storage.MY_BLOCKED_TITLE_KEYWORDS}>
+          my blocked title keywords
+        </label>
+        <p>
+          comma separated words; if a post title includes this word(s) it will
+          not be shown
+        </p>
+        <textarea
+          defaultValue={myBlockedTitleKeywords}
+          id={Storage.MY_BLOCKED_TITLE_KEYWORDS}
+          name={Storage.MY_BLOCKED_TITLE_KEYWORDS}
+          onChange={() => setIsSaved(false)}
+        />
+      </fieldset>
 
-        <button className="editor__btn" type="submit">
-          Save
-        </button>
-        {isSaved && <p>Changes were saved.</p>}
-      </form>
-    </main>
+      <button className="editor__btn" type="submit">
+        Save
+      </button>
+      {isSaved && <p>Changes were saved.</p>}
+    </form>
   )
 }
 
