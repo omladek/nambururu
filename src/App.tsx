4cf6ff5fb0d021179aabc6a3f464455b008cb499
Ondrej Mladek <ondrej.mladek@gmail.com>
2023-07-01 13:12:53 +0200

    lazy load routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { JSX, createContext } from 'preact'
 import { Router, Route } from 'preact-router'
 import { createHashHistory } from 'history'
-
+import AsyncRoute from 'preact-async-route'
 import { useState } from 'preact/hooks'
+
 import Home from './routes/Home'
-import Settings from './routes/Settings'
 import hashHistoryAdapter from './utilities/hashHistoryAdapter'
 import Header from './components/Header/Header'
-import EditList from './components/Editor/components/EdditList/EditList'
-import EditBlockedSubreddits from './components/Editor/components/EditBlockedSubreddits/EditBlockedSubreddits'
-import EditBlockedTitleKeywords from './components/Editor/components/EditBlockedTitleKeywords/EditBlockedTitleKeywords'
-import Lists from './components/Editor/components/Lists/Lists'
+import Loader from './components/Loader'
 
 export const NavigationContext = createContext(window.location.href)
 
@@ -29,14 +27,49 @@ function App(): JSX.Element {
         onChange={handleRouteChange}
       >
         <Route component={Home} default path="/:subreddit?" />
-        <Route component={Settings} path="/settings/" />
-        <Route component={Lists} path="/lists/" />
-        <Route component={EditBlockedSubreddits} path="/blocked-subreddits/" />
-        <Route
-          component={EditBlockedTitleKeywords}
+        <AsyncRoute
+          getComponent={() =>
+            import('./routes/Settings').then((module) => module.default)
+          }
+          loading={() => <Loader isFullScreen />}
+          path="/settings/"
+        />
+        <AsyncRoute
+          getComponent={() =>
+            import('./components/Editor/components/Lists/Lists').then(
+              (module) => module.default,
+            )
+          }
+          loading={() => <Loader isFullScreen />}
+          path="/lists/"
+        />
+        <AsyncRoute
+          getComponent={() =>
+            import(
+              './components/Editor/components/EditBlockedSubreddits/EditBlockedSubreddits'
+            ).then((module) => module.default)
+          }
+          loading={() => <Loader isFullScreen />}
+          path="/blocked-subreddits/"
+        />
+        <AsyncRoute
+          getComponent={() =>
+            import(
+              './components/Editor/components/EditBlockedTitleKeywords/EditBlockedTitleKeywords'
+            ).then((module) => module.default)
+          }
+          loading={() => <Loader isFullScreen />}
           path="/blocked-title-keywords/"
         />
-        <Route component={EditList} path="/edit/:list?" />
+        <AsyncRoute
+          getComponent={() =>
+            import('./components/Editor/components/EdditList/EditList').then(
+              (module) => module.default,
+            )
+          }
+          loading={() => <Loader isFullScreen />}
+          path="/edit/:list?"
+        />
       </Router>
     </NavigationContext.Provider>
   )
