96c6c4ed75dfa7b9065c4399884274a3f34264de
Ondrej Mladek <ondrej.mladek@gmail.com>
2023-07-02 11:37:20 +0200

    update unit test

--- src/utilities/__tests__/deescapeHtml.test.ts
+++ src/utilities/__tests__/deescapeHtml.test.ts
@@ -8,4 +8,8 @@ describe('deescapeHtml', () => {
       deescapeHtml(`Foo &amp; bar, &lt;a&gt;link&lt;/a&gt; &quot; &#039;`),
     ).eq(`Foo & bar, <a>link</a> " &#039;`)
   })
+
+  it('returns empty string', () => {
+    expect(deescapeHtml('')).eq('')
+  })
 })
